535a294d9bea51a10d7fbd2f9c09a373090e1bf2
moshe beeri <moshe.beeri@gmail.com>
2021-09-29 21:46:00 +0300

    UI Pack

---
+++ Assets/Modern and Clean UI Pack/Scripts/slider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class slider : MonoBehaviour
+{
+    public TextMeshProUGUI sliderValue;
+    public void ValueChange()
+    {
+       sliderValue.text = this.gameObject.GetComponent<Slider>().value.ToString("0.0");
+    }
+}
